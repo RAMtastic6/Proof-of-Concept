c0eda351e0ada60ce2ff852dd0ea45b0e0739f5f
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-04-03 14:12:35 +0200

    Commentato il codice con le cose da fare e aggiunto un db temporaneo per poter capire il funzionamento del server.

--- websocket-server/src/gateway/gateway.ts
+++ websocket-server/src/gateway/gateway.ts
@@ -5,6 +5,7 @@ import { OnModuleInit } from "@nestjs/common";
 @WebSocketGateway()
 export class MyGateway implements OnModuleInit {
 
+    // questi sono tutti dati solamente a scopo dimostrativo
     db = {
         "id_prenotazione_1" : {
             "piatto1" : 0,
@@ -34,54 +35,60 @@ export class MyGateway implements OnModuleInit {
     onModuleInit() {
         this.server.on('connection', (socket) => {
             console.log(socket.id + " connected");
-            // serve per poter rispondere al client con un messaggio di acknowledgement
-            // una volta che il messaggio di acknowledgement è stato ricevuto dal client
-            // il client può richiedere il menu associato alla prenotazione.
 
-            // trovare un modo di passare dati dal client durante la connessione
-            // console.log(socket.handshake.query['id_prenotazione']);
-            // if (socket.handshake.query != null) { // significa che ci stiamo connettendo al server socket.
-            //     const id_prenotazione: string = socket.handshake.query.id_prenotazione.toString(); 
-            //     this.server.to(socket.id).emit('onConnection', { prenotazione : this.db[id_prenotazione]});
-            // }
+
+            if (socket.handshake.query["id_prenotazione"] != null) { // significa che ci stiamo connettendo al server socket.
+            //     TODO
+            //     const result = await this.reservationRepository.find({ ... })
+                const id_prenotazione: string = socket.handshake.query.id_prenotazione.toString(); 
+                console.log(id_prenotazione);
+                this.server.to(socket.id).emit('onMessage', { prenotazione : this.db[id_prenotazione]});
+            }
         });
     }
     
 
-    @SubscribeMessage('getMenu')
-    onGetMenu(@MessageBody() body: any) {
-        const id = body.id;
-        console.log(this.db[id]);
-        this.server.emit('onReceiveMenu', {
-            "prenotazione" : this.db[body['id_prenotazione_1']]
-        })
-    }
+    // body deve avere questo formato per il momento:
+    // {
+    //     "id_ordinazione" : string
+    //     "plate" : string,
+    //     "quantity" : number
+    // } 
 
     @SubscribeMessage('increment')
     async onIncrement(@MessageBody() body: any) {
-        console.log(body["plate"]);
-
+        
         // TODO 
         // const result = await this.reservationRepository.find({ ... })
-        const result = await (await fetch("https://jsonplaceholder.typicode.com/todos/1")).json();
         
-        this.piatti[body["plate"]] += result["id"];
-        console.log(this.piatti);
+        const prenotazione : string = body["id_prenotazione"];
+        const piatto : string = body["plate"];
+        const inc : number = body["quantity"]
+
+        this.db[prenotazione][piatto] += inc;
+        console.log(this.db);
 
         this.server.emit('onMessage', {
-            "ordine" : this.piatti
+            "ordine" : this.db[prenotazione]
         })
     }
 
     @SubscribeMessage('decrement')
-    onDecrement(@MessageBody() body: any) {
-        console.log(body["plate"]);
+    async onDecrement(@MessageBody() body: any) {
 
-        this.piatti[body["plate"]] -= body["quantity"]
-        console.log(this.piatti);
+        // TODO 
+        // const result = await this.reservationRepository.find({ ... })
+
+        const prenotazione : string = body["id_prenotazione"];
+        const piatto : string = body["plate"];
+        const dec : number = body["quantity"]
+
+        this.db[prenotazione][piatto] -= dec;
+        console.log(this.db);
 
         this.server.emit('onMessage', {
-            "ordine" : this.piatti
-        })      
+            "ordine" : this.db[prenotazione]
+        })
+              
     }
 }
