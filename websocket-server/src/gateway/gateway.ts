91666ac6b512aabec24ffcb3aefe1addfb7b5438
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-04-05 17:05:16 +0200

    Tolti commenti inutili.

--- websocket-server/src/gateway/gateway.ts
+++ websocket-server/src/gateway/gateway.ts
@@ -6,9 +6,7 @@ import { OnModuleInit } from "@nestjs/common";
 export class MyGateway implements OnModuleInit {
 
     // questi sono tutti dati solamente a scopo dimostrativo
-    db = {
-
-    }
+    db = {}
 
     @WebSocketServer()
     server: Server;
@@ -25,32 +23,23 @@ export class MyGateway implements OnModuleInit {
             console.log(socket.id + " connected");
 
             if (socket.handshake.query["id_prenotazione"] != null) { // significa che ci stiamo connettendo al server socket.
+                // debug
                 console.log(this.db);
-                //     TODO
-                //     const result = await this.reservationRepository.find({ ... })
                 const id_prenotazione: string = socket.handshake.query.id_prenotazione.toString();
-                //TODO: this.server.to(socket.id).emit('onMessage', this.db[id_prenotazione]);
+                // TODO: this.server.to(socket.id).emit('onMessage', this.db[id_prenotazione]);
             }
         });
 
-        // TODO: fare le chiamate per poter inizializzare
-        // i vari repository utili a TypeORM per poter
     }
-
-
-    // body deve contenere il menu:
 
     @SubscribeMessage('increment')
     async onIncrement(@MessageBody() body: any) {
 
-        // TODO 
-        // const result = await this.reservationRepository.find({ ... })
-
         const prenotazione: string = body["id_prenotazione"];
         /*const piatto: string = body["plate"];
         const inc: number = body["quantity"];
-
         this.db[prenotazione][piatto] += inc;*/
+
         this.db[prenotazione]["menu"] = body["menu"];
         console.log(this.db[prenotazione]["menu"]);
         this.server.emit('onMessage', this.db[prenotazione]["menu"]);
@@ -62,20 +51,15 @@ export class MyGateway implements OnModuleInit {
     @SubscribeMessage('decrement')
     async onDecrement(@MessageBody() body: any) {
 
-        // TODO 
-        // const result = await this.reservationRepository.find({ ... })
-
         const prenotazione: string = body["id_prenotazione"];
         /*
         const piatto: string = body["plate"];
         const dec: number = body["quantity"]
-
         this.db[prenotazione][piatto] -= dec; */
 
         this.db[prenotazione]["menu"] = body["menu"];
         console.log(this.db[prenotazione]["menu"]);
         this.server.emit('onMessage', this.db[prenotazione]["menu"]);
-
 
         // debug
         console.log(this.db);
